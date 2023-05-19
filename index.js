38f6a777714b4792816abc1551df58ed4406ec73
IH Shadin <shadinfr@yahoo.com>
2023-05-19 23:04:34 +0600

    delete function added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -49,6 +49,12 @@ async function run() {
         app.post('/add-toy', async (req, res) => {
             const newToy = req.body;
             const result = await toysCollection.insertOne(newToy);
+            res.send(result);
+        })
+        app.delete('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.deleteOne(query);
             res.send(result);
         })
 
